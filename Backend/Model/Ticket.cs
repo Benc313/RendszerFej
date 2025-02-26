72f9c3dccdd959144f8a86e78cb755f4d2d97702
Kuruc Attila <attilakuruc12@gmail.com>
2025-02-26 19:26:37 +0100

    Added Ticket class

---
+++ Backend/Model/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Model
+{
+    public class Ticket
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int SeatNumber { get; set; }
+
+        [Required]
+        
+        public int Price { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(32)")]
+        public string Status { get; set; }
+
+        
+
+    }
+}
