9aa00ceb95e1d235c1dcd2d3f72ab3ef0e40d6da
b3nec <b3necv@gmail.com>
2025-02-26 19:32:05 +0100

    Terem, Screening added :3

---
+++ Backend/Model/Screening.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Model
+{
+    public class Screening
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime ScreeningDate { get; set; }
+
+
+    }
+}
