bd33f2d8134bfd99c0e670db04a219adb53e8369
Ati61 <lehartattila6@gmail.com>
2025-03-05 11:56:34 +0100

    movieResponse for getMovie, movieResponse for Createmovie (idk hogy jo e de azert pusholom)

--- Backend/Controllers/MovieController.cs
+++ Backend/Controllers/MovieController.cs
@@ -17,34 +17,48 @@ public class MovieController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult> GetMovies() //GETMOVIEEESS
+    public async Task<ActionResult> GetMovies() 
     {
         return Ok(await _db.Movies.ToListAsync());
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult> GetMovie(int id) //GETMOVIE
+    public async Task<ActionResult<MovieResponse>> GetMovie(int id)
     {
-        var movie = await _db.Movies.FindAsync(id);
+        Movie? movie = await _db.Movies.FindAsync(id);
         if (movie == null)
             return NotFound(); // 404
-        else if (movie is not Movie)
-            return BadRequest("Wrong type");
 
-        return Ok(movie);
+        var movieResponse = new MovieResponse
+        {
+            Id = movie.Id,
+            Title = movie.Title,
+            Description = movie.Description,
+            Duration = movie.Duration,
+            //idk kell e screenings ide
+        };
+
+        return Ok(movieResponse);
     }
 
     [HttpPost]
-    [Authorize(Roles = "Admin")]
-    public async Task<ActionResult> CreateMovie(Movie movie) //CREATEMOVIE
+    //[Authorize(Roles = "Admin")]
+    public async Task<ActionResult<Movie>> CreateMovie(MovieRequest movieRequest) //screening nelkul create movie
     {
+        var movie = new Movie
+        {
+            Title = movieRequest.Title,
+            Description = movieRequest.Description,
+            Duration = movieRequest.Duration
+        };
+
         _db.Movies.Add(movie);
         await _db.SaveChangesAsync();
         return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, movie);
     }
 
     [HttpPut("{id}")]
-    [Authorize(Roles = "Admin")]
+    //[Authorize(Roles = "Admin")]
     public async Task<ActionResult> UpdateMovie(int id, Movie movie) //UPDATEMOVIE
     {
         if (id != movie.Id)
@@ -68,7 +82,7 @@ public class MovieController : ControllerBase
     }
 
     [HttpDelete("{id}")]
-    [Authorize(Roles = "Admin")]
+    //[Authorize(Roles = "Admin")]
     public async Task<ActionResult> DeleteMovie(int id) //DELETEMOVIE
     {
         var movie = await _db.Movies.FindAsync(id);
@@ -87,4 +101,19 @@ public class MovieController : ControllerBase
     {
         return _db.Movies.Any(x => x.Id == id);
     }
+    
+    public class MovieResponse
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public uint Duration { get; set; }
+    }
+    public class MovieRequest
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public uint Duration { get; set; }
+    }
+  
 }
