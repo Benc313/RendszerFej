1a0130b1ec5fc1cab7002b7f4890503b53967e55
Ati61 <lehartattila6@gmail.com>
2025-04-06 22:03:09 +0200

    feat: Implement OrderController with CRUD operations for managing orders and ticket purchases TODO: make it work

---
+++ Backend/Messages/OrderResponse.cs
@@ -0,0 +1,21 @@
+﻿using Backend.Model;
+
+namespace Backend.Messages;
+
+public class OrderResponse
+{
+    public int Id { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public int UserId { get; set; }
+    public List<int> TicketIds { get; set; } = new List<int>();
+
+    public OrderResponse(Orders order)
+    {
+        Id = order.Id;
+        Phone = order.Phone;
+        Email = order.Email;
+        UserId = order.UserId;
+        TicketIds = order.Tickets.Select(t => t.Id).ToList();
+    }
+}
