27bdcd4ceada3702afc6a67a38857122179019ab
Horváth Bence <bencus@his.hu>
2025-04-07 20:26:04 +0200

    feat: Implement TicketController with ticket validation functionality

---
+++ Backend/Controllers/TicketController.cs
@@ -0,0 +1,35 @@
+using expenseTracker.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers;
+[ApiController]
+[Route("/tickets")]
+public class TicketController : ControllerBase
+{
+	private readonly dbContext _db;
+
+	public TicketController(dbContext db)
+	{
+		_db = db;
+	}
+
+	[HttpGet("validate/{id}")]
+	public async Task<ActionResult> ValidateTicket(int id)
+	{
+		//only cashier and admin can validate tickets
+		var ticket = await _db.Tickets.FirstOrDefaultAsync(t => t.Id == id);
+		if (ticket == null)
+		{
+			return NotFound(new { Errors = new List<string> { "Ticket not found" } });
+		}
+		
+		if(ticket.Status == "VALIDATED")
+		{
+			return BadRequest(new { Message = "Ticket already validated" });
+		}
+		ticket.Status = "VALIDATED";
+		await _db.SaveChangesAsync();
+		return Ok(new { Errors = new List<string> { "Ticket already validated" } });
+	}
+}
